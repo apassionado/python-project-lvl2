e0b4dbf10f7abea849f03b8232ac39bf1027eef6
Vitaly Kobalchinskij <v9i9t9@yahoo.com>
2022-09-12 13:15:46 +0300

    Added --help description

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def main():
+    """Compares two configuration files and shows a difference."""
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument("first_file", type=str)
+    parser.add_argument("second_file", type=str)
+    parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
