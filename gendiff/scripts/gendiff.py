1e4f4499718c6257a809de0987b5e6f301a086bd
Vitaly Kobalchinskij <v9i9t9@yahoo.com>
2022-09-13 22:45:22 +0300

    Add optional arg in -h

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import argparse
 
 
@@ -6,6 +7,7 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument("first_file", type=str)
     parser.add_argument("second_file", type=str)
+    parser.add_argument("-f", "--format", help="set format of output")
     parser.parse_args()
 
 
